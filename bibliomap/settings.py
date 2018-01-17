d183d5bc2d2b679d61072e57efa56fc223fe15a1
Unknown <hugofer93@gmail.com>
2018-01-17 01:09:41 -0500

    configuracion finalizada
    proyecto listo para ser puesto en produccion, configurado para usar S3, RDS y EC2.

--- bibliomap/settings.py
+++ bibliomap/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages'
+    'storages',
     'bibliomap',
 ]
 
